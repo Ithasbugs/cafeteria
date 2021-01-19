edc5d2f358bf5b9690dc765aef3ce498332ad521
ithasbugs <ithasbugs@gmail.com>
2021-01-19 22:49:18 +0530

    Designed DB to model conversion

--- SAG_Cafeteria/SAG_Cafeteria/settings.py
+++ SAG_Cafeteria/SAG_Cafeteria/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '(rt6j@qnpw-lo-s6n9kun^=qq64twtl39cm-=09y95-jv%mjxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Cafeteriacore'
 ]
 
 MIDDLEWARE = [
